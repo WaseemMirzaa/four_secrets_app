64a21fc71ac5c43dd4577b272655b4a2451d7057
CODING-MONKEY\Patri <patrick.schubert.1@gmx.de>
2025-07-21 21:56:10 +0200

    is now muenchner_geheimtip...

--- lib/data/muenchner_geheimtipp_routes.dart
+++ lib/data/muenchner_geheimtipp_routes.dart
@@ -3,7 +3,6 @@ import 'package:four_secrets_wedding_app/routes/routes.dart';
 class MuenchnerGeheimtippRoutes {
   static List getRoutes() {
     final List<String> routes = [
-      RouteManager.bachelorettePartyPage,
       RouteManager.hairMakeUpPage,
       RouteManager.brauBraeutigamPage,
       RouteManager.locationPage,
@@ -14,8 +13,6 @@ class MuenchnerGeheimtippRoutes {
       RouteManager.traurednerPage,
       RouteManager.fotographPage,
       RouteManager.tanzschulePage,
-      RouteManager.kosmetischeAkupunkturPage,
-      RouteManager.personalTrainingPage,
       RouteManager.papeteriePage,
       RouteManager.unterhaltungPage,
       RouteManager.trauringePage,
