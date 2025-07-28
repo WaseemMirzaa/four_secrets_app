45ad859d5ad706aa3bc9734c75697175431fb600
CODING-MONKEY\Patri <patrick.schubert.1@gmx.de>
2025-07-28 14:45:52 +0200

    Do we need further?

--- lib/pages/coolab_details_screen.dart
+++ lib/pages/coolab_details_screen.dart
@@ -1,3 +1,5 @@
+// Do we need further?
+
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:flutter/material.dart';
