62d3ef7a666220921107f0e2657126283fb543db
CODING-MONKEY\Patri <patrick.schubert.1@gmx.de>
2025-08-07 01:09:20 +0200

    change image paths

--- lib/data/muenchner_geheimtipp_images.dart
+++ lib/data/muenchner_geheimtipp_images.dart
@@ -2,22 +2,22 @@ class MuenchnerGeheimtippImages {
   // List with all assets images
   static List<String> getImages() {
     final List<String> images = [
-      'assets/images/inspirations/Bacheloretteparty.jpg',
-      'assets/images/inspirations/Hair_und_MakeUp_Styling.jpg',
-      'assets/images/inspirations/Braut_Braeutigam_Atelier.jpg',
-      'assets/images/inspirations/Location.jpg',
-      'assets/images/inspirations/Gesang.jpg',
-      'assets/images/inspirations/Band_DJ.jpg',
-      'assets/images/inspirations/Florist_Floristin.jpg',
-      'assets/images/inspirations/Patiserie.jpg',
-      'assets/images/inspirations/Trauredner.jpg',
-      'assets/images/inspirations/Fotograf_Fotografin.jpg',
-      'assets/images/inspirations/Tanzschule.jpg',
-      'assets/images/inspirations/Kosmetische_Akupunktur.jpg',
-      'assets/images/inspirations/Personal_Training.jpg',
-      'assets/images/inspirations/Papeterie.jpg',
-      'assets/images/inspirations/Unterhaltung.jpg',
-      'assets/images/inspirations/Trauringe.jpg'
+      'assets/images/muenchner_geheimtipps/Bacheloretteparty.jpg',
+      'assets/images/muenchner_geheimtipps/Hair_und_MakeUp_Styling.jpg',
+      'assets/images/muenchner_geheimtipps/Braut_Braeutigam_Atelier.jpg',
+      'assets/images/muenchner_geheimtipps/Location.jpg',
+      'assets/images/muenchner_geheimtipps/Gesang.jpg',
+      'assets/images/muenchner_geheimtipps/Band_DJ.jpg',
+      'assets/images/muenchner_geheimtipps/Florist_Floristin.jpg',
+      'assets/images/muenchner_geheimtipps/Patiserie.jpg',
+      'assets/images/muenchner_geheimtipps/Trauredner.jpg',
+      'assets/images/muenchner_geheimtipps/Fotograf_Fotografin.jpg',
+      'assets/images/muenchner_geheimtipps/Tanzschule.jpg',
+      'assets/images/muenchner_geheimtipps/Kosmetische_Akupunktur.jpg',
+      'assets/images/muenchner_geheimtipps/Personal_Training.jpg',
+      'assets/images/muenchner_geheimtipps/Papeterie.jpg',
+      'assets/images/muenchner_geheimtipps/Unterhaltung.jpg',
+      'assets/images/muenchner_geheimtipps/Trauringe.jpg'
     ];
     return images;
   }
